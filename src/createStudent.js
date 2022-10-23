59420e7e2952407aefa5312c74c93b349bf9f082
Vinicius José <viniciusjosedev@gmail.com>
2022-10-23 14:09:51 -0300

    requisito 5 finalizado

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,16 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+function feedback() {
+  return 'Eita pessoa boa!';
+}
 
+const createStudent = (name) => { 
+  if (name === '') {
+    return undefined;
+  } return { name, 
+    feedback };
+};
+const estudante = createStudent('Leandrão, o Lobo Solitário');
+console.log(typeof estudante.feedback);
 module.exports = createStudent;
