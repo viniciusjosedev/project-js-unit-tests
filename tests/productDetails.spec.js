5557eff18a94cb324a77f2e4cdc814ab7ccac87d
Vinicius José <viniciusjosedev@gmail.com>
2022-10-24 07:22:20 -0300

    requisito 6 finalizado

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -30,14 +30,39 @@ const productDetails = require('../src/productDetails');
 */
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
-  it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    fail('Teste vazio!');
-    // ESCREVA SEUS TESTES ABAIXO:
-    // Teste se productDetails é uma função.
-    // Teste se o retorno da função é um array.
-    // Teste se o array retornado pela função contém dois itens dentro.
-    // Teste se os dois itens dentro do array retornado pela função são objetos.
-    // Teste se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si.
-    // Teste se os dois productIds terminam com 123.
-  });
+  it('Teste se productDetails é uma função', () => {
+		expect(typeof productDetails).toBe('function');
+	});
+	it('Teste se o retorno da função é um array', () => {
+		expect(typeof productDetails('maisena', 'leite condensado')).toEqual('object');
+	});
+	it('Teste se o array retornado pela função contém dois itens dentro.', () => {
+	  expect(productDetails('maise', 'leite de condensado').length).toEqual(2)
+	});
+  it("Teste se os dois itens dentro do array retornado pela função são objetos.", () => {
+		expect(typeof productDetails('maisa', 'leite condensado')[0] && typeof productDetails('maisa', 'leite condensado')[1]).toEqual('object')
+	})
+  it("Teste se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si.", () => {
+		expect(productDetails('maisa', 'leite condensado')[0]['name'] && productDetails('maisa', 'leite condensado')[1]['name']).not.toEqual(productDetails('maisa', 'leite condensado')[0])
+	})
+  it('Teste se os dois productIds terminam com 123.', () => {
+		const verific = () => {
+			const final = productDetails('maisa', 'leite condensado');
+			let texto = ''
+			for (let index = final[0]['details']['productId'].length - 3; index < final[0]['details']['productId'].length; index += 1) {
+				texto += final[0]['details']['productId'][index];
+			}
+			let texto2 = ''
+			for (let index = final[1]['details']['productId'].length - 3; index < final[1]['details']['productId'].length; index += 1) {
+				texto2 += final[1]['details']['productId'][index];
+			}
+			if (texto === texto2) {
+				return true; 
+			} else {
+				return false;
+			}
+		}
+		expect(verific()).toEqual(true)
+	})
 });
+
